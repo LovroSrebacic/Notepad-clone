42416a4eeae2ff24725b8b9378f4cbf844876f6f
Lovro Srebačić <ls51575@fer.hr>
2024-03-21 01:42:31 +0100

    Added location classes for a cursor inside the text editor

---
+++ src/location/Location.java
@@ -0,0 +1,67 @@
+package location;
+
+public class Location {
+	private int x;
+	private int y;
+	
+	public Location(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Location(Location location) {
+		this.x = location.getX();
+		this.y = location.getY();
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public Location getLocation() {
+		return this;
+	}
+	
+	public void setLocation(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setLocation(Location location) {
+		this.x = location.getX();
+		this.y = location.getY();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + this.x + "," + this.y + ")";
+	}
+}
